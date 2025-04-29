dc753d1e9015e38c89e1f2a4d9391532330dc1da
suo3 <samuel.o.asare@gmail.com>
2025-04-28 20:20:06 -0400

    update the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "VLOG | Video Feed",
+  description: "Video blog website built with Next.js and Supabase",
 };
 
 const geistSans = Geist({
